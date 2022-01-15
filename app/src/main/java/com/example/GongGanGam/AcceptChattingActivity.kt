44a4253c983d14b377c69684c775d84a977dbd34
임수진 <sjlim@imsujin-ui-MacBookAir.local>
2022-01-16 03:49:34 +0900

    받은 답장 액티비티 ui 완료 및 거절버튼 누르면 footer 사라지게 구현

---
+++ app/src/main/java/com/example/GongGanGam/AcceptChattingActivity.kt
@@ -0,0 +1,30 @@
+package com.example.GongGanGam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.transition.Visibility
+import android.view.View
+import com.example.GongGanGam.databinding.ActivityAcceptChattingBinding
+
+class AcceptChattingActivity : AppCompatActivity() {
+    lateinit var binding: ActivityAcceptChattingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAcceptChattingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initListener()
+    }
+
+    private fun initListener() {
+        binding.acceptChattingBackIv.setOnClickListener {
+            finish()
+        }
+
+        binding.acceptChattingRejectBtn.setOnClickListener {
+            // 거절 누르고 -> 거절 상태 저장
+            binding.acceptChattingFooter.visibility = View.GONE
+        }
+    }
+}
