720661acb5044e02a2b6886261453d3de9e3d473
Soyeon <soyunsarang@naver.com>
2022-01-24 00:59:15 +0900

    Feat Edit User API & Fix errors

--- config/baseResponseStatus.js
+++ config/baseResponseStatus.js
@@ -62,7 +62,6 @@ module.exports = {
     // 6000번 소연
     USER_BIRTHYEAR_EMPTY : {"isSuccess": false, "code": 6000, "message": "출생년도 값을 입력해주세요."},
     USER_GENDER_EMPTY : {"isSuccess": false, "code": 6001, "message": "성별을 입력해주세요."},
-    USER_STATUS_EMPTY : {"isSuccess": false, "code": 6002, "message": "상태를 입력해주세요."},
     USER_DIARY_PUSH_EMPTY : {"isSuccess": false, "code": 6003, "message": "받은일기 알림 설정을 선택해주세요."},
     USER_ANSWER_PUSH_EMPTY : {"isSuccess": false, "code": 6004, "message": "받은답장 알림 설정을 선택해주세요."},
     USER_CHAT_PUSH_EMPTY : {"isSuccess": false, "code": 6005, "message": "채팅 알림 설정을 선택해주세요."},
