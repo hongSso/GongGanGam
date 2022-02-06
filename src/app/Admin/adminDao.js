54bd38728547b1dfc8588e76f4b1e6dd3197e444
SY-Mina <sylim9387@naver.com>
2022-02-06 22:39:15 +0900

    Feat Paging Notice API

--- src/app/Admin/adminDao.js
+++ src/app/Admin/adminDao.js
@@ -1,9 +1,20 @@
 
 // 공지사항 조회
-async function selectNoticeList(connection) {
+async function selectNoticeList(connection, pageSize, offset) {
+    const params = [pageSize, offset]
     const selectNoticeQuery = `
         select title, noticeContent, date_format(updatedAt, '%Y.%c.%e') as noticeDate
-        from Notice;
+        from Notice order by updatedAt limit ? offset ?;
+    `;
+    const [noticeRows] = await connection.query(selectNoticeQuery, params);
+    return noticeRows;
+}
+
+// 모든 공지사항 조회
+async function selectAllNoticeList(connection) {
+    const selectNoticeQuery = `
+        select title, noticeContent, date_format(updatedAt, '%Y.%c.%e') as noticeDate
+        from Notice order by updatedAt;
     `;
     const [noticeRows] = await connection.query(selectNoticeQuery);
     return noticeRows;
@@ -11,6 +22,6 @@ async function selectNoticeList(connection) {
 
 
 module.exports = {
-    selectNoticeList
+    selectNoticeList, selectAllNoticeList
 };
 
--- src/app/Admin/adminProvider.js
+++ src/app/Admin/adminProvider.js
@@ -8,7 +8,16 @@ const adminDao = require("./adminDao");
 
 exports.retrieveNotice = async function (pageSize, offset) {
     const connection = await pool.getConnection(async (conn) => conn);
-    const noticeList = await adminDao.selectNoticeList(connection, );
+    const noticeList = await adminDao.selectNoticeList(connection, pageSize, offset);
+
+    connection.release();
+
+    return noticeList;
+};
+
+exports.retrieveAllNotice = async function () {
+    const connection = await pool.getConnection(async (conn) => conn);
+    const noticeList = await adminDao.selectAllNoticeList(connection);
 
     connection.release();
 
